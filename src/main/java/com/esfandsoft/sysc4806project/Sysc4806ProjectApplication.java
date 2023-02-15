e60757a4e727cd9e38d7d0688a6a7925f44c40a8
Curtis <110percent@riseup.net>
2023-02-15 18:04:10 -0500

    Initial content commit

---
+++ src/main/java/com/esfandsoft/sysc4806project/Sysc4806ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.esfandsoft.sysc4806project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sysc4806ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sysc4806ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esfandsoft/sysc4806project/Sysc4806ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfandsoft.sysc4806project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sysc4806ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
