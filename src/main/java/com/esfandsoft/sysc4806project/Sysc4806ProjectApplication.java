be5ae66c5a17352d1108ce83a947aac3c6def262
110Percent <110percent@riseup.net>
2023-03-01 16:49:48 -0500

    Add HTTP Session support

--- src/main/java/com/esfandsoft/sysc4806project/Sysc4806ProjectApplication.java
+++ src/main/java/com/esfandsoft/sysc4806project/Sysc4806ProjectApplication.java
@@ -3,8 +3,10 @@ package com.esfandsoft.sysc4806project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableJdbcHttpSession
 public class Sysc4806ProjectApplication {
 
     public static void main(String[] args) {
