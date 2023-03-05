aca9df0e89a99cd4045f6b4f369f6fbd5f264940
Curtis <110percent@riseup.net>
2023-03-04 20:24:50 -0500

    Add login support

---
+++ src/main/java/com/esfandsoft/sysc4806project/objects/UserLoginDto.java
@@ -0,0 +1,22 @@
+package com.esfandsoft.sysc4806project.objects;
+
+public class UserLoginDto {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
