8fc8fa318de2b3d8e6c47f6a0181c90f223ec45f
Nicholas <nars@rogers.com>
2023-03-21 22:02:55 -0400

    fix security flaws in the results controllers

--- src/main/java/com/esfandsoft/sysc4806project/controllers/ResultsController.java
+++ src/main/java/com/esfandsoft/sysc4806project/controllers/ResultsController.java
@@ -1,12 +1,22 @@
 package com.esfandsoft.sysc4806project.controllers;
 
+import com.esfandsoft.sysc4806project.entities.Survey;
+import com.esfandsoft.sysc4806project.entities.User;
 import com.esfandsoft.sysc4806project.repositories.SurveyRepository;
 import com.esfandsoft.sysc4806project.repositories.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/results")
@@ -17,6 +27,8 @@ public class ResultsController {
 
     @Autowired
     UserRepository userRepository;
+
+    Logger logger = LogManager.getLogger(ResultsController.class);
 
     /**
      * Default Page for Results
@@ -41,10 +53,46 @@ public class ResultsController {
      * @author Nicholas Sendyk, 101143602
      */
     @GetMapping("/{id}")
-    public String viewSurveyResults(@PathVariable long id) {
-        // TODO: Ensure user has privileges to view the results page
+    public String viewSurveyResults(@PathVariable long id, HttpSession session, Model model) {
 
-        return "survey_results";
+        // If not logged in, send user to the landing page
+        if (session.getAttribute("username") == null) {
+            return "landing";
+        }
+
+        // Get the user from the database to load the list of surveys
+        Optional<User> fetchedUser = userRepository.findByUsername(String.valueOf(session.getAttribute("username")));
+        if (fetchedUser.isEmpty()) {
+            // This shouldn't happen, but just in case...
+            logger.error("User was logged in with username" + session.getAttribute("username") +
+                    ", but the user entry was not found in the database?");
+            logger.info("Invalidating session for user");
+            session.invalidate();
+            return "landing";
+        }
+
+        // Get the users surveys from the database
+        Collection<Survey> s = fetchedUser.get().getSurveys();
+        ArrayList<Long> s_ids = new ArrayList<>();
+        for (Survey s1 : s) {
+            s_ids.add(s1.getId());
+        }
+
+        // if the user is the owner of the survey
+        if (s_ids.contains(id)) {
+            Survey fetchedSurvey = surveyRepository.findById(id);
+            // if the survey is closed
+            if (fetchedSurvey.getIsClosed()) {
+                return "survey_results";
+            }
+        }
+
+        User user = fetchedUser.get();
+        model.addAttribute("username", user.getUsername());
+        ArrayList<Survey> surveys = new ArrayList<>(user.getSurveys());
+        model.addAttribute("surveys", surveys);
+
+        return "dashboard";
     }
 
 }
